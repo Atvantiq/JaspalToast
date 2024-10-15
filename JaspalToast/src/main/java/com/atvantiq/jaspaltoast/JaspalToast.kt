31dec5fefe3d06dea93b9bbecce77a9be99bc474
Atvantiq <Atvantiq@gmail.com>
2024-10-15 13:07:36 +0530

    init

--- JaspalToast/src/main/java/com/atvantiq/jaspaltoast/JaspalToast.kt
+++ JaspalToast/src/main/java/com/atvantiq/jaspaltoast/JaspalToast.kt
@@ -1,13 +1,20 @@
 package com.atvantiq.jaspaltoast
 
 import android.content.Context
+import android.content.Intent
 import android.view.Gravity
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 
 class JaspalToast {
     fun showToast(context: Context, message : String ){
         val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
         toast.setGravity(Gravity.CENTER, 0, 0)
         toast.show()
+    }
+
+    fun initMyViewActivity(context: Context){
+        var intent = Intent(context,MyViewActivity::class.java)
+        startActivity(context,intent,null)
     }
 }
--- JaspalToast/src/main/java/com/atvantiq/jaspaltoast/MyViewActivity.kt
+++ JaspalToast/src/main/java/com/atvantiq/jaspaltoast/MyViewActivity.kt
@@ -1,17 +1,16 @@
-package com.atvantiq.customtoast
+package com.atvantiq.jaspaltoast
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.atvantiq.jaspaltoast.JaspalToast
 
-class MainActivity : AppCompatActivity() {
+class MyViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_my_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
